df08a5ed8c3132899f6c6e7983e1a6952638205d
blanq31 <obiwulugodswill@gmail.com>
2023-08-21 19:44:53 +0100

    finished another section

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -1,5 +1,10 @@
 import picOne from '../cargo-images/pic-one.png';
 import picTwo from '../cargo-images/pic-two.png';
+import phone from '../cargo-images/phone.png';
+import behind from '../cargo-images/behind.png';
+import top from '../cargo-images/top.png';
+import picThree from '../cargo-images/pic-three.png';
+import { FaGooglePlay, FaAppStore } from 'react-icons/fa';
 import Button from './Button';
 
 
@@ -28,6 +33,44 @@ const Content = () => {
               <Button />
             </div>
         </div>
+      </section>
+
+      <section className='w-full my-8 flex items-center justify-center'>
+        <div className='hidden md:flex items-center justify-center gap-8 bg-primary w-[80%] rounded-lg px-8 py-4 relative'>
+            <div className='relative z-10'>
+              <img src={phone}></img>
+            </div>
+            
+            <div className='w-[50%]'>
+              <h1 className='text-white font-bold text-4xl mb-4'>Get Our Mobile App</h1>
+
+              <p className='text-white mb-4'>Get 50% discount on your first domestic delivery on our mobile app for the first time. Download    for Android and IOS on Google Play store and App Store
+              </p>
+
+              <div className='flex items-center justify-start gap-4'>
+                  <button className='text-white flex justify-around items-center p-4 border-2 gap-2 rounded-lg border-white hover:translate-y-3 duration-500 ease-in-out'>Get on IOS<FaAppStore/></button>
+                  <button className='text-white flex justify-around items-center p-4 border-2 gap-2 rounded-lg border-white hover:translate-y-3 duration-500 ease-in-out'>Get on Andriod<FaGooglePlay/></button>
+              </div>
+
+              <img src={behind} className='absolute bottom-0 left-0'/>
+              <img src={top} className='absolute right-0 top-0'/>
+             
+            </div>
+        </div>
+      </section>
+
+      <section className='w-full my-4'>
+        <div className='flex flex-col-reverse md:flex-row items-center justify-around md:gap-4'>
+            <div>
+                  <img src={picThree}></img>
+            </div>
+            <div className='md:w-[50%] h-[50vh] px-8 md:px-0 flex flex-col items-start justify-center'>
+                      <h1 className='text-primary font-bold text-3xl md:text-5xl md:w-[60%] mb:-2 md:mb-8'>Stay updated, Every step of the way</h1>
+                      <p className='w-[80%] md:w-[60%] mb-2 md:mb-8 text-gray-500'>Our tracking system lets you follow your package’s progress from pickup to delivery, ensuring you’re always in the loop</p>
+                      <Button />
+              </div>
+        </div>
+
       </section>
 
       <section></section>
